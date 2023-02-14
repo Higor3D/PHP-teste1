970d52f5eb1ca02bbbd32cc7d7844a23dc41dff3
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-02-13 22:07:54 -0300

    commit

--- action/sign_in.php
+++ action/sign_in.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    require_once db_conection.sql;
+    require_once 'db_connection.php';
 
     if(isset($_POST['btn-cadastrar'])){
         $nome = mysqli_escape_string($connection,$_POST['name']);
